c55510e272615102b9a2fbae76735e2f370b28a3
Antonis Mazeras <sdi1400092@di.uoa.gr>
2021-12-01 23:19:07 +0200

    HT_init

---
+++ code/src/hash_file.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "bf.h"
+#include "hash_file.h"
+#define MAX_OPEN_FILES 20
+
+#define CALL_BF(call)       \
+{                           \
+  BF_ErrorCode code = call; \
+  if (code != BF_OK) {         \
+    BF_PrintError(code);    \
+    return HP_ERROR;        \
+  }                         \
+}
+
+typedef struct {
+  Record **item;
+  int local_depth;
+}buckets;
+
+typedef struct {
+  buckets **bucket;
+  int global_depth;
+}HashTable;
+
+HT_ErrorCode HT_Init() {
+  //insert code here
+  HashTable *HT;
+  HT = (HashTable*)malloc(sizeof(HashTable));
+  if (HT != NULL){
+    HT->bucket = NULL;
+    HT->global_depth=0;
+    return HT_OK;
+  }
+  return HT_ERROR;
+}
+
+HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
+  //insert code here
+  return HT_OK;
+}
+
+HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
+  //insert code here
+  return HT_OK;
+}
+
+HT_ErrorCode HT_CloseFile(int indexDesc) {
+  //insert code here
+  return HT_OK;
+}
+
+HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
+  //insert code here
+  return HT_OK;
+}
+
+HT_ErrorCode HT_PrintAllEntries(int indexDesc, int *id) {
+  //insert code here
+  return HT_OK;
+}
+

c0231b0e59f8daad9fc7f59493919c24a5ee1aeb
giannis-ml <giannismallios2001@gmail.com>
2021-12-04 12:28:15 +0200

    .

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -11,13 +11,14 @@
   BF_ErrorCode code = call; \
   if (code != BF_OK) {         \
     BF_PrintError(code);    \
-    return HP_ERROR;        \
+    return HT_ERROR;        \
   }                         \
 }
 
 typedef struct {
   Record **item;
   int local_depth;
+  int maxSize;
 }buckets;
 
 typedef struct {
@@ -26,30 +27,35 @@ typedef struct {
   int global_depth;
 }HashTable;
 
+int Open_files[MAX_OPEN_FILES]=NULL;
+
+
+
 HT_ErrorCode HT_Init() {
-  //insert code here
+  //insert code here  
   return HT_OK;
 }
 
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
-  //insert code here
+  int id;
+  CALL_BF(BF_CreateFile(filename));
   HashTable *HT;
-  HT = (HashTable*)malloc(sizeof(HashTable));
-  if (HT != NULL){
-    strcpy(HT->file_name, filename);
-    HT->bucket = NULL;
-    HT->global_depth = depth;
-    printf("HT created with filename: %s\n", HT->file_name);
-    return HT_OK;
-  }
-  return HT_ERROR;
+
 }
 
 HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
   //insert code here
+  int i;
+  CALL_BF(BF_OpenFile(fileName,&indexDesc));
+  for(i=0 ; i < MAX_OPEN_FILES ; i++){
+    if(Open_files[i] != NULL){
+      Open_files[i] = indexDesc;
+      break;
+    }
+  }
   return HT_OK;
 }
-
+ 
 HT_ErrorCode HT_CloseFile(int indexDesc) {
   //insert code here
   return HT_OK;
@@ -57,7 +63,6 @@ HT_ErrorCode HT_CloseFile(int indexDesc) {
 
 HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
   //insert code here
-  return HT_OK;
 }
 
 HT_ErrorCode HT_PrintAllEntries(int indexDesc, int *id) {
